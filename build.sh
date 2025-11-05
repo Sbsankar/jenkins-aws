6d560bb670035117a100f9cdb2c0d275cf21ca06
Ubuntu <ubuntu@ip-172-31-33-79.ap-south-1.compute.internal>
2025-11-05 10:23:49 +0000

    Testing the webhook

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ echo "echo 'Build successful! Hello from EC2!'"
 echo "echo 'Build was triggered by commit: $GIT_COMMIT'" 
 echo "echo 'Job: $JOB_NAME, Build: $BUILD_NUMBER'"
 echo "echo '---------------------------------'"
-New line by Aravind
+echo "echo 'new testing by aravind'"
